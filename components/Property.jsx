07489c69c439daf32929c9ed70e811f7c8d8a283
Juan Eulufi Salazar <92868937+BotUsuario@users.noreply.github.com>
2022-10-07 19:13:17 -0300

    mostrando fotos y precios

--- components/Property.jsx
+++ components/Property.jsx
@@ -1,17 +1,63 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import {Box, Flex, Text, Avatar} from './@chakra-ui/react'
-import {FaBed, FaBath} from 'react-icons/fa'
-import {BsGridFill} from 'react-icons/bs'
-import {GoVerified} from 'react-icons/go'
-import millify from 'millify'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
+import { FaBed, FaBath } from "react-icons/fa";
+import { BsGridFill } from "react-icons/bs";
+import { GoVerified } from "react-icons/go";
+import millify from "millify";
 
-export default function Property({property: {coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalId}}) {
-  return (  
+import defaultImage from "../assets/images/house.jpg";
+
+export default function Property({
+  property: {
+    coverPhoto,
+    price,
+    rentFrequency,
+    rooms,
+    title,
+    baths,
+    area,
+    agency,
+    isVerified,
+    externalId,
+  },
+}) {
+  return (
     <Link href={`/property/${externalId}`} passHref>
-      {title}
+      <Flex
+        flexWrap="wrap"
+        w="420px"
+        p="5"
+        paddingTop="0"
+        justifyContent="flex-start"
+        cursor="pointer"
+      >
+        <Box>
+          <Image
+            src={coverPhoto ? coverPhoto.url : defaultImage}
+            alt="house"
+            width={400}
+            height={260}
+          />
+        </Box>
+        <Box w="full">
+          <Flex
+            paddingTop="2"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Flex alignItems="center">
+              <Box paddingRight="3" color="green.400">
+                {isVerified && <GoVerified />}
+              </Box>
+              <Text fontWeight='bold' fontSize='lg'>
+                AED {price} {rentFrequency && `/${rentFrequency}`}
+              </Text>
+            </Flex>
+          </Flex>
+        </Box>
+      </Flex>
     </Link>
-  )
+  );
 }
-
